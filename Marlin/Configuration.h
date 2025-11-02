acf05a1a9942402d48af0d7f9709c5fdacd17d62
Oleksandr <shalal545@gmail.com>
2025-11-02 12:31:18 +0200

    enable preheat and increase bed leveling to 5x5

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2172,10 +2172,10 @@
 /**
  * Auto-leveling needs preheating
  */
-//#define PREHEAT_BEFORE_LEVELING
+#define PREHEAT_BEFORE_LEVELING
 #if ENABLED(PREHEAT_BEFORE_LEVELING)
   #define LEVELING_NOZZLE_TEMP 120   // (°C) Only applies to E0 at this time
-  #define LEVELING_BED_TEMP     50
+  #define LEVELING_BED_TEMP     80
 #endif
 
 /**
@@ -2228,7 +2228,7 @@
 #if ANY(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 3
+  #define GRID_MAX_POINTS_X 5
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
